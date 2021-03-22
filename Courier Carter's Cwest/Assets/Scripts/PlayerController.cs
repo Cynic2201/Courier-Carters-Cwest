9e8d5e91ec786a16b3a09dc81ed72ba6659b44c7
pokemon2201 <62266523+pokemon2201@users.noreply.github.com>
2021-03-22 06:36:14 -0400

    s

---
+++ Courier Carter's Cwest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    //public Rigidbody RB;
+    public CharacterController charController;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RB = GetComponent<Rigidbody>();
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, RB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        /*if (Input.GetButtonDown("Jump"))
+        {
+            RB.velocity = new Vector3(RB.velocity.x, jumpForce, RB.velocity.z);
+        }*/
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+}
