87cf6aba88f31a73027c9d05280f5f18ad44f8fc
tstack2 <tstack2@uncc.edu>
2021-05-12 23:05:25 -0400

    TIMER!!!

---
+++ Courier Carter's Cwest/Assets/Scripts/TextTimer2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TextTimer2 : MonoBehaviour
+{
+	TextMesh text;
+	float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent("TextMesh") as TextMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		time += Time.deltaTime;
+        text.text = Math.Round(time, 2) + "\n / 60";
+    }
+}
