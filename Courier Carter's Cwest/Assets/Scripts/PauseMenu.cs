f9b66dda4ac4d8fa840d4af44e192ab9f92ad6fe
pokemon2201 <62266523+pokemon2201@users.noreply.github.com>
2021-05-12 22:02:24 -0400

    Menu

--- Courier Carter's Cwest/Assets/Scripts/PauseMenu.cs
+++ Courier Carter's Cwest/Assets/Scripts/PauseMenu.cs
@@ -12,7 +12,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if(Input.GetKeyDown(KeyCode.Escape))
             if(Paused)
             {
                 Resume();
