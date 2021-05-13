95f9b07e325d4a408171ecac37edd757fa521f47
tstack2 <tstack2@uncc.edu>
2021-05-12 22:32:09 -0400

    timersorta

---
+++ Courier Carter's Cwest/Assets/Scripts/TextTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTimer : MonoBehaviour
+{
+	TextMesh text;
+	float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent("TextMesh") as TextMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		time += Time.deltaTime;
+        text.text = "Your Time: " + time;
+    }
+}
